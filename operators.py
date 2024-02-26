52b104c604c6e2ee653f44c6e11a9f64ffe98763
lbh <3558506522@qq.com>
2024-02-26 14:59:47 +0800

    product类的基本操作已经完成

--- operators.py
+++ operators.py
@@ -10,6 +10,9 @@ conn = mysql.connector.connect(
 # 获取游标对象
 cursor = conn.cursor()
 
+# 产品表
+table1 = 'products'
+
 # 产品类
 class Product:
     def __init__(self, product_id, name, description, price):
@@ -18,21 +21,57 @@ class Product:
         self.description = description
         self.price = price
 
-    def add_product(self, table):
-        sql = f'insert into {table} values(%s, %s, %s, %s)'
+    # 添加产品
+    def add_product(self):
+        sql = f'insert into {table1} values(%s, %s, %s, %s)'
         cursor.execute(sql, (self.product_id, self.name, self.description, self.price))
         # 提交更改
         conn.commit()
 
-# # 库存类
-# class Inventory:
-#     def __init__(self):
-#         self.products = []
-#
-#
+    # 删除产品  提供根据id和name进行某一行的删除操作
+    def delete_product_fromID(self):
+        sql = f'delete from {table1} where product_id = {self.product_id}'
+        cursor.execute(sql)
+        conn.commit()
+
+    def delete_product_fromNAME(self):
+        sql = f'delete from {table1} where name = {self.name}'
+        cursor.execute(sql)
+        conn.commit()
+
+    # 修改定价 id / name
+    def modify_price_fromID(self, new_price):
+        sql = f'update {table1} set price = {new_price} where product_id = {self.product_id}'
+        cursor.execute(sql)
+        conn.commit()
+
+    def modify_price_fromNAME(self, new_price):
+        sql = f'update {table1} set price = {new_price} where name = {self.name}'
+        cursor.execute(sql)
+        conn.commit()
+
+    # 获取产品信息
+    def show_product_info(self):
+        sql = f'select price from {table1} where product_id = {self.product_id}'
+        cursor.execute(sql)
+        result_price = cursor.fetchall()
+
+        sql2 = f'select name from {table1} where product_id = {self.product_id}'
+        cursor.execute(sql2)
+        result_name = cursor.fetchall()
+
+        sql3 = f'select description from {table1} where product_id = {self.product_id}'
+        cursor.execute(sql3)
+        result_desc = cursor.fetchall()
+        # 输出信息
+        print(f'{self.product_id}：{result_name}的价格是{result_price}，它的基础信息是{result_desc}')
+
 
 p1 = Product('123', 'apple', 'ashda', 12.3)
-p1.add_product('products')
+p1.add_product()
+p1.show_product_info()
+p1.modify_price_fromID(15.5)
+p1.show_product_info()
 
 # 关闭连接
 cursor.close()
